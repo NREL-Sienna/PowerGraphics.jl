150a955a046da2ebae863161432ad10ce82bace7
Jose Daniel Lara <jdlara@berkeley.edu>
2020-02-20 09:21:32 -0700

    update toml

---
+++ test/runtests.jl
@@ -0,0 +1,91 @@
+using Test
+using Logging
+using Dates
+import InteractiveUtils
+
+import InfrastructureSystems
+import InfrastructureSystems: Deterministic, Probabilistic, ScenarioBased, Forecast
+const IS = InfrastructureSystems
+using PowerSystems
+import PowerSystems: PowerSystemTableData
+const PSY = PowerSystems
+
+@test 1 = 1
+
+#= Enable once the tests are properly developed
+
+macro includetests(testarg...)
+    if length(testarg) == 0
+        tests = []
+    elseif length(testarg) == 1
+        tests = testarg[1]
+    else
+        error("@includetests takes zero or one argument")
+    end
+
+    quote
+        tests = $tests
+        rootfile = @__FILE__
+        if length(tests) == 0
+            tests = readdir(dirname(rootfile))
+            tests = filter(
+                f ->
+                        startswith(f, "test_") &&
+                        endswith(f, ".jl") && f != basename(rootfile),
+                tests,
+            )
+        else
+            tests = map(f -> string(f, ".jl"), tests)
+        end
+        println()
+        for test in tests
+            print(splitext(test)[1], ": ")
+            include(test)
+            println()
+        end
+    end
+end
+
+function get_logging_level(env_name::String, default)
+    level = get(ENV, env_name, default)
+    log_level = get(LOG_LEVELS, level, nothing)
+    if log_level == nothing
+        error("Invalid log level $level: Supported levels: $(values(LOG_LEVELS))")
+    end
+
+    return log_level
+end
+
+function run_tests()
+    console_level = get_logging_level("PS_CONSOLE_LOG_LEVEL", "Error")
+    console_logger = ConsoleLogger(stderr, console_level)
+    file_level = get_logging_level("PS_LOG_LEVEL", "Info")
+
+    IS.open_file_logger(LOG_FILE, file_level) do file_logger
+        levels = (Logging.Info, Logging.Warn, Logging.Error)
+        multi_logger =
+            IS.MultiLogger([console_logger, file_logger], IS.LogEventTracker(levels))
+        global_logger(multi_logger)
+
+        # Testing Topological components of the schema
+        @time @testset "Begin PowerSystems tests" begin
+            @includetests ARGS
+        end
+
+        # TODO: once all known error logs are fixed, add this test:
+        #@test length(get_log_events(multi_logger.tracker, Logging.Error)) == 0
+
+        @info IS.report_log_summary(multi_logger)
+    end
+end
+
+logger = global_logger()
+
+try
+    run_tests()
+finally
+    # Guarantee that the global logger is reset.
+    global_logger(logger)
+    nothing
+end
+=#
