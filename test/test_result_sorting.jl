6bd607304e196683fe8c7df988bd93575f76b625
Barrows <clayton.barrows@nrel.gov>
2020-12-31 06:10:10 -0700

    cleanup

--- test/test_result_sorting.jl
+++ test/test_result_sorting.jl
@@ -1,14 +1,3 @@
-#=
-@testset "test results sorting" begin
-    namez = collect(names(IS.get_variables(res)[:P__ThermalStandard]))
-    Variabless = Dict(:P__ThermalStandard => [namez[1], namez[2]])
-    sorted = PG.sort_data(res; Variables = Variabless)
-    sorted_two = PG.sort_data(res)
-    sorted_names = [namez[1], namez[2]]
-    @test names(IS.get_variables(sorted)[:P__ThermalStandard]) == sorted_names
-    @test names(IS.get_variables(sorted_two)[:P__ThermalStandard]) == sort(namez)
-end
-=#
 (results_uc, results_ed) = run_test_sim(TEST_RESULT_DIR)
 
 @testset "test filter results" begin
